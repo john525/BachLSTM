d262f6ee24bb0da117ff03416545aed512a033ca
john525 <john.lhota.525@gmail.com>
2019-11-26 18:38:40 -0500

    changed syntax to work with music21 tracks, removed note on-off combining fn based on amy's comment, todo: one hot encode each token

--- preprocessing.py
+++ preprocessing.py
@@ -5,19 +5,11 @@ from music21 import midi as midi
 
 MAX_NOTE = 128
 MAX_CHANNEL = 16
-MAX_NOTE_LEN = 600 # TODO: check
 MAX_VELOCITY = 128 # TODO: check
 
-def find_note_end(note_offs, note, channel, time):
-    for msg in midi_file:
-        if msg.type is 'note_off' or (msg.type is 'note_on' and msg.velocity == 0):
-            if msg.time > time and msg.channel == channel and msg.note == note:
-                return msg.time
-    return midi_file.length
-
-def note_to_vec(note, channel, length, velocity):
-    one_hot = np.zeros((MAX_NOTE, MAX_CHANNEL, MAX_NOTE_LEN, MAX_VELOCITY))
-    idx = int(note), int(channel), int(length), int(velocity)
+def note_to_vec(note, channel, velocity):
+    one_hot = np.zeros((MAX_NOTE, MAX_CHANNEL, MAX_VELOCITY))
+    idx = int(note), int(channel), int(velocity)
     one_hot[idx] = 1.0
     return one_hot
 
@@ -28,7 +20,7 @@ def load_data(path_to_midi_files, all_data=True):
     """
     files = [fname for fname in os.listdir(path_to_midi_files) if fname not in 'sankey bwv988 bwv232 leipzig']
     num_tokens = 0
-    vector_tracks = []
+    songs = []
 
     word_limit = 1.6e6
     if all_data == False:
@@ -36,21 +28,23 @@ def load_data(path_to_midi_files, all_data=True):
     num_words = 0
 
     for i,fname in enumerate(files):
-        vector_track = []
-
+        print('reading file %d out of %d...' % (i+1, len(files)))
         midi_file = midi.MidiFile()
-        midi_file.open(filename=os.path.join(path_to_midi_files, fname))
+        midi_file.open(os.path.join(path_to_midi_files, fname), 'rb')
+        midi_file.read()
+        midi_file.close()
 
-        # TODO: how to ticks per beat?
+        # TODO: normalize ticks per beat
 
-        # Combine every note_on and note_off into a single token, then concatenate based on start time
+        # Convert midi song to a vector
+        song_vector = []
         for channel in midi_file.tracks:
-            notes = channel.get_piano_roll(fs=30)
-            print(notes)
-        vector_tracks.append(vector_track)
-        num_words += len(vector_track)
+            song_vector = [event for event in channel.events] # TODO: represent each event as a one-hot
+        songs.append(song_vector)
+        num_words += len(song_vector)
+
 
         if num_words >= word_limit:
             break # Huang and Wu's dataset had 1.6 million words
 
-    return vector_tracks
+    return songs
