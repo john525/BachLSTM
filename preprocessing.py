d2f524f81544582aa9c3a0c1c5384c26acec0ae8
john525 <john.lhota.525@gmail.com>
2019-12-06 03:19:53 -0500

    change processing to use unique tokens for one hot generation

--- preprocessing.py
+++ preprocessing.py
@@ -8,27 +8,31 @@ MAX_PITCH = 128
 MAX_CHANNEL = 16
 MAX_VELOCITY = 128 # TODO: check
 
-def vectorize_event(evt):
+def tokenize_event(evt, token_dict):
     one_hot = np.zeros((2, MAX_PITCH, MAX_CHANNEL, MAX_VELOCITY))
     msg_type = int(evt.type == 'NOTE_ON')
     idx = msg_type, int(evt.pitch), int(evt.channel), int(evt.velocity)
-    one_hot[idx] = 1.0
-    return one_hot
+    if idx not in token_dict:
+        token_dict[idx] = len(token_dict)
+    return token_dict[idx]
 
 def load_data(path_to_midi_files, all_data=True):
     """
     path_to_midi_files - path to the dataset containing .mid files
-    returns - a list of note vectors
+    returns - a list of note vectors, and a token dictionary
     """
     files = [fname for fname in os.listdir(path_to_midi_files) if fname not in 'sankey bwv988 bwv232 leipzig']
     num_tokens = 0
+
     songs = []
+    token_dict = {}
 
     if all_data == False:
         files = files[:10]
 
+    print()
     for i,fname in enumerate(files):
-        print('reading file %d out of %d...' % (i+1, len(files)))
+        print('\r\r Reading file %d out of %d...' % (i+1, len(files)))
         midi_file = midi.MidiFile()
         midi_file.open(os.path.join(path_to_midi_files, fname), 'rb')
         midi_file.read()
@@ -41,8 +45,11 @@ def load_data(path_to_midi_files, all_data=True):
         for channel in midi_file.tracks:
             for event in channel.events:
                 if event.type in 'NOTE_ON NOTE_OFF':
-                    vec = vectorize_event(event)
-                    song_vector.append(vec)
+                    song_vector.append(tokenize_event(event, token_dict))
         songs.append(song_vector)
 
-    return songs
+    print('\nUnique Tokens: %d' % len(token_dict))
+    for i in range(len(songs)):
+        songs[i] = tf.one_hot(songs[i], len(token_dict))
+        
+    return songs, {token_dict[data]: data for data in token_dict}
