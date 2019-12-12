52291c9853ab985e21755ad5ec5615f2c08cca33
john525 <john.lhota.525@gmail.com>
2019-12-12 15:32:12 -0500

    add count_data_tokens

--- preprocessing.py
+++ preprocessing.py
@@ -63,3 +63,48 @@ class MidiLoader:
             songs[i] = tf.one_hot(songs[i], len(token_dict))
 
         return songs, labels, {token_dict[data]: data for data in token_dict}
+
+    def count_data_tokens(self, path_to_midi_files, all_data=True):
+        """
+        path_to_midi_files - path to the dataset containing .mid files
+        returns - a list of note vectors, and a token dictionary
+        """
+        files = [fname for fname in os.listdir(path_to_midi_files) if fname not in 'sankey bwv988 bwv232 leipzig']
+        num_tokens = 0
+
+        songs = []
+        token_dict = {}
+
+        if all_data == False:
+            files = files[:5]
+        elif self.file_index >= len(files):
+            return None, None, None
+        else:
+            files = files[self.file_index : min(self.file_index + 5, len(files))]
+            self.file_index += 5
+
+        print()
+        for i,fname in enumerate(files):
+            print('\r\r Reading file %d out of %d...' % (i+1, len(files)))
+            midi_file = midi.MidiFile()
+            midi_file.open(os.path.join(path_to_midi_files, fname), 'rb')
+            midi_file.read()
+            midi_file.close()
+
+            # TODO: normalize ticks per beat
+
+            # Convert midi song to a vector
+            song_vector = [] # TODO: represent each event as a one-hot, remove metadata events
+            for channel in midi_file.tracks:
+                for event in channel.events:
+                    if event.type in 'NOTE_ON NOTE_OFF':
+                        idx = event.type, int(event.pitch), int(event.channel), int(event.velocity)
+                        if idx not in token_dict:
+                            token_dict[idx] = 1
+                        else:
+                            token_dict[idx] = 1 + token_dict[idx]
+
+        keys = list(token_dict.values())
+        keys.sort()
+        for k in keys:
+            print(k)
